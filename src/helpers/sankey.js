6afe1d367e52b392da96c00297986e891c30e8db
Mark Jenkins <mark.jenkins@dynamicaction.com>
2018-10-25 15:13:56 -0500

    initial git commit

--- src/helpers/sankey.js
+++ src/helpers/sankey.js
@@ -1,69 +1,58 @@
 const asyncHandler = require('express-async-handler')
+
+//Validation schemas used by ajv module to validate our mistakes
 const feedFlowSchema = require('/opt/nexus/src/validations/feedFlow.json');
 const feedOutputsSchema = require('/opt/nexus/src/validations/feedOutputs.json');
 const fragInputsSchema = require('/opt/nexus/src/validations/fragInputs.json');
+
+//Read a yaml asynchronously
 var readYaml = require('read-yaml-promise');
+
 var Ajv = require('ajv');
 var ajv = new Ajv({allErrors: true});
 require('ajv-async')(ajv);
 
 module.exports = asyncHandler(async (req, res, next) => {
+  //Read in our YAML files
   const feedFlows = await readYaml('/opt/nexus/config/' + req.params.feedid + '.yaml');
   const fragInputs = await readYaml('/opt/nexus/config/fragInputs.yaml');
   const feedOutputs = await readYaml('/opt/nexus/config/feedOutputs.yaml');
 
+  //Validate our feed flow schem and throw an error if we fail
   var validate = ajv.compile(feedFlowSchema);
   var validateResults = validate(feedFlows);
-
   if(! validateResults) {
-    let errString = 'The feed flow file fails validation:<br>';
-    validate.errors.forEach(function(errorItem) {
-      errString = errString + '<br>' + errorItem.dataPath + ' ' + errorItem.message;
-    });
-    let err = new Error(errString);
-    //console.log(validate.errors);
-    err.status = 200;
-    return next(err);
+    return validationError('The feed flow file fails validation.', validate.errors);
   }
 
+  //Validate our feedOutputs schema and throw an error if we fail
   validate = ajv.compile(feedOutputsSchema);
   validateResults = validate(feedOutputs);
-
   if(! validateResults) {
-    let errString = 'The feed outputs file fails validation:<br>';
-    validate.errors.forEach(function(errorItem) {
-      errString = errString + '<br>' + errorItem.dataPath + ' ' + errorItem.message;
-    });
-    let err = new Error(errString);
-    //console.log(validate.errors);
-    err.status = 200;
-    return next(err);
+    return validationError('The feed outputs file fails validation', validate.errors);
   }
 
   validate = ajv.compile(fragInputsSchema);
   validateResults = validate(fragInputs);
-
   if(! validateResults) {
-    let errString = 'The frag inputs file fails validation:<br>';
-    validate.errors.forEach(function(errorItem) {
-      errString = errString + '<br>' + errorItem.dataPath + ' ' + errorItem.message;
-    });
-    let err = new Error(errString);
-    //console.log(validate.errors);
-    err.status = 200;
-    return next(err);
+    return validationError('The frag inputs file fails validation.', validate.errors);
   }
 
   var nodes = [];
   var links = [];
   var flowData = {};
   flowData['flowInfo'] = {};
-  //Assign numerical values to all of our nodes
+
+  //Assign numerical values to all of our nodes.
+  //This is required for the Sankey diagram
   var nodeValueCounter = 0;
+  //loop through all flows of the feed job
   for (var flowIndex in feedFlows['flows']) {
     var flow = feedFlows['flows'][flowIndex];
+    //loop through all the flowInputs of each flow
     for (var flowInputIndex in flow.flowInputs) {
       var flowInput = flow.flowInputs[flowInputIndex];
+      //this is a new flow input we are discovering. Assign it a nodevalue number
       if (! flowData.flowInfo.hasOwnProperty(flowInput)) {
         flowData['flowInfo'][flowInput]={};
         flowData['flowInfo'][flowInput]['nodeValue'] = nodeValueCounter;
@@ -73,68 +62,78 @@ module.exports = asyncHandler(async (req, res, next) => {
           if (flowInput == fragInputs['fragInputs'][fragInputIndex]['id']) {
             flowData['flowInfo'][flowInput]['html']=getFragmentHTML(flowInput);
             flowData['flowInfo'][flowInput]['flowhtml']=getFragmentFlowHTML(flowInput);
+            //Core fragments get a weight of 4, non-core of 1
             if (fragInputs['fragInputs'][fragInputIndex]['isCore'] == true) {
-              flowData['flowInfo'][flowInput]['weight'] = 6;
+              flowData['flowInfo'][flowInput]['weight'] = 4;
             } else {
               flowData['flowInfo'][flowInput]['weight'] = 1;
             }
           }
         }
+        //Increment our node value counter for the next discovered node
         nodeValueCounter++;
       }
     }
+    //Track that we have found a new flow as well
     if (! flowData.flowInfo.hasOwnProperty(flow.flowId)) {
       flowData['flowInfo'][flow.flowId]={};
       flowData['flowInfo'][flow.flowId]['nodeValue'] = nodeValueCounter;
+      flowData['flowInfo'][flow.flowId]['html'] = getFlowLogicHTML(flow.flowId);
+      flowData['flowInfo'][flow.flowId]['flowhtml'] = getFlowHTML(flow.flowId);
       nodes.push({name: flow.flowId});
       nodeValueCounter++;
     }
   }
 
   //Assign weights to the flows based on inputs.
-  //Core fragments have a weight of 30
-  //Mapping fragments have a weight of 10
+  //We already assigned fragment weights above, so assuming
+  //no errors or cycles we should be able to calculate all weights
+  // of the flows
 
   var haveAllWeights = false;
   var iterations = 1;
   var missingCalcs = [];
+
+  //I am willing to try 100 times. If we can't do it after that then
+  //we probably have a cycle
   while (iterations <= 100 && haveAllWeights === false) {
-    //console.log('Starting Iteration:' + iterations);
+
     missingCalcs = [];
     //Assume we have them all until we dont
     haveAllWeights = true;
-    //console.log(feedFlows['flows']);
+
+    //Loop through our flows
     for (flowIndex in feedFlows['flows']) {
       flow = feedFlows['flows'][flowIndex]
       var flowId = flow['flowId'];
       var weight = 0;
+      //Assume we have weights for all inputs until we don't
+      //If we do we can easily sum those up to get our weight
       var allInputsHaveWeights = true;
       for (flowInputIndex in flow['flowInputs']) {
         flowInput = flow['flowInputs'][flowInputIndex];
-        //console.log(flowId + ' has ' + flowInput + ' at  (' + flowInputIndex + ')');
+
+        //We already know the weight of this one, so move on
         if (flowData['flowInfo'][flowId].hasOwnProperty('weight')) {
           allInputsHaveWeights = true;
         } else {
           //We need to try to calculate the weight if possible
           //Does my flowInput even have a weight
           if(flowData['flowInfo'][flowInput].hasOwnProperty('weight')) {
-            //console.log(flowId + ' has weight of ' + weight);
             weight = weight + flowData['flowInfo'][flowInput]['weight'];
-            //console.log(flowId + ' has new weight of ' + weight);
           } else {
-            //console.log('Not enough info to calculate the weight for ' + flowId + '( Missing: ' + flowInput + ' )');
+            //We can't calculate this one yet, so track what's missing
             missingCalcs.push('Not enough info to calculate the weight for ' + flowId + '( Missing: ' + flowInput + ' )');
             allInputsHaveWeights = false;
             haveAllWeights = false;
           }
         }
       }
+      //Set a weight if we've just calculated one
       if (allInputsHaveWeights === true &&
        ! flowData['flowInfo'][flowId].hasOwnProperty('weight')) {
         //console.log('Calculated wieght for ' + flowId + ' (' + weight + ')');
         flowData['flowInfo'][flowId]['weight'] = weight;
-        flowData['flowInfo'][flowId]['flowhtml'] = getFlowHTML(flowId);
-        flowData['flowInfo'][flowId]['html'] = '';
       }
     }
     iterations++;
@@ -144,11 +143,11 @@ module.exports = asyncHandler(async (req, res, next) => {
       return next(err);
     }
   }
-  //console.log(flowData);
-  //Build the nodes and links
+
+  //Build we create the links which is just an index value
+  //to another index value with the calculated weight
   feedFlows['flows'].forEach(function(flow) {
     flow['flowInputs'].forEach(function(flowInput) {
-      //{"source":0,"target":1,"value":10},
       var link = {};
       link.source=flowData['flowInfo'][flowInput]['nodeValue'];
       link.target=flowData['flowInfo'][flow['flowId']]['nodeValue'];
@@ -157,22 +156,24 @@ module.exports = asyncHandler(async (req, res, next) => {
     });
   });
 
+  //Attach all necessary data to the flowData array
   flowData.nodes = nodes;
   flowData.links = links;
   flowData.feedName = feedFlows.feedJobName;
   flowData.feedOutput = feedFlows.feedOutput;
+  flowData.sankeyWidth = feedFlows.sankeyWidth;
 
   res.json(flowData);
   next();
 
+  //Function that returns HTML content for our fragment input modal
   function getFragmentHTML(fragment) {
-
     var fragmentHTML = '';
     fragInputs['fragInputs'].forEach(function(fragInput) {
       if (fragment === fragInput['id']) {
         fragmentHTML = '<p>' + fragInput['description'] + '</p>';
         fragmentHTML = fragmentHTML + '<p>Below is the schema for this input fragment</p>';
-        fragmentHTML = fragmentHTML + '<table class="table table-condensed"><thead><tr><th>Column Name</th><th>Column Type</th></tr></thead>';
+        fragmentHTML = fragmentHTML + '<table class="table table-sm" style="font-size:12px;"><thead><tr><th>Column Name</th><th>Column Type</th></tr></thead>';
         fragInput['schema'].forEach(function(column) {
           fragmentHTML = fragmentHTML + '<tbody><tr><td>' + column['columnName'] + '</td><td>' + column['columnType'] + '</td></tr>';
         });
@@ -182,12 +183,13 @@ module.exports = asyncHandler(async (req, res, next) => {
     return fragmentHTML;
   }
 
+  //Function that returns the fragment flow HTML for our fragment flow input modal
   function getFragmentFlowHTML(fragment) {
     var fragmentHTML = '';
     fragInputs['fragInputs'].forEach(function(fragInput) {
       if (fragment === fragInput['id']) {
         fragmentHTML = fragmentHTML + '<p>Below is the schema output of ' + fragInput['id'] + '</p>';
-        fragmentHTML = fragmentHTML + '<table class="table table-condensed"><thead><tr><th>Column Name</th><th>Column Type</th></tr></thead>';
+        fragmentHTML = fragmentHTML + '<table class="table table-sm" style="font-size:12px;"><thead><tr><th>Column Name</th><th>Column Type</th></tr></thead>';
         fragInput['schema'].forEach(function(column) {
           fragmentHTML = fragmentHTML + '<tbody><tr><td>' + column['columnName'] + '</td><td>' + column['columnType'] + '</td></tr>';
         });
@@ -197,10 +199,11 @@ module.exports = asyncHandler(async (req, res, next) => {
     return fragmentHTML;
   }
 
+  //Function that returns the normal flow HTML
   function getFlowHTML(flowId) {
     //console.log(flowId);
     var flowHTML = '<p>Below is the schema output of ' + flowId + '</p>';
-    flowHTML = flowHTML + '<table class="table table-condensed"><thead><tr><th>Column Name</th><th>Column Type</th></tr></thead>';
+    flowHTML = flowHTML + '<table class="table table-sm" style="font-size:12px;"><thead><tr><th>Column Name</th><th>Column Type</th></tr></thead>';
     feedFlows['flows'].forEach(function(flow) {
       if(flowId === flow['flowId']) {
         if(flow.hasOwnProperty('schema')) {
@@ -211,5 +214,21 @@ module.exports = asyncHandler(async (req, res, next) => {
       }
     });
     return flowHTML;
+  }
+
+  //Function that returns the flow logic HTML for our nodes
+  function getFlowLogicHTML(flowId) {
+    return flowId;
+  }
+
+  //Just a helper function to help validate some data
+  function validationError(errTitle, errors) {
+    let errString = errTitle;
+    errors.forEach(function(errorItem) {
+      errString = errString + '<br>' + errorItem.dataPath + ' ' + errorItem.message;
+    });
+    let err = new Error(errString);
+    err.status = 200;
+    return next(err);
   }
 });
--- src/static/dna_sankey.js
+++ src/static/dna_sankey.js
@@ -2,50 +2,10 @@
 //This is code that runs on the client side in the client browser, not server code
 
 function drawFeedSankey(feedId) {
-  //
+  //Set the title for the diagram
   $('#myFeedJobTitle').html('<h4>' + feedId + '</h4>');
+  //Empty out the contents of the container (in case we are drawing again)
   $('#mySankeyDigram').html('');
-
-  // Variables to hold sizing of the sankey
-  var margin = {top: 0, right: 100, bottom: 20, left: 0},
-    width = 2200 - margin.left - margin.right,
-    height = 300 - margin.top - margin.bottom;
-
-
-  // format variables
-  var color = d3.scaleOrdinal(d3.schemeCategory20);
-
-  // I'll be honest. I have no clue here. 
-  var zoomListener = d3.zoom()
-    .scaleExtent([1, 5])
-    .on('zoom', zoomed);
-
-  function zoomed() {
-    if (d3.event.transform.k === 1) d3.event.transform.y = 0;
-    if (d3.event.transform.x > 0) d3.event.transform.x = 0;
-    svg.attr('transform', d3.event.transform);
-  }
-
-  // append the svg object to the body of the page
-  var svg = d3.select('#mySankeyDigram').append('svg')
-    .attr('width', width + margin.left + margin.right)
-    .attr('height', height + margin.top + margin.bottom)
-    .append('g')
-    .attr('transform',
-          'translate(' + margin.left + ',' + margin.top + ')')
-
-  zoomListener(svg);
-
-  // Set the sankey diagram properties
-  var sankey = d3.sankey()
-    .nodeWidth(10)
-    .nodePadding(30)
-    .size([width, height])
-    .overlapLinksAtSources(true)
-    //.overlapLinksAtTargets(true)
-    .align('right');
-
-  var path = sankey.link();
 
   // Show our work in progress div
   var myprogress = $('#work-in-progress');
@@ -56,130 +16,173 @@ function drawFeedSankey(feedId) {
 
     // If we got an error back we need to display the error and move on with life
     if(graph.hasOwnProperty('error')) {
-      
-      // We aren't working in progress any longer
-      var myprogress = $('#work-in-progress');
-      myprogress.hide();
-
-      // Enable the alert so we see the error on the site
-      var myalert = $('#myAlert');
-      myalert.find('strong').html('Validation Error!');
-      myalert.find('p').html(graph.error);
-      myalert.removeAttr('class');
-      myalert.addClass('alert alert-danger alert-dismissable');
-      myalert.show();
-      return;
+      return divError('Validation Error!', graph.error);
     }
+
+    console.log(graph);
+    var sankeyWidth = (graph.hasOwnProperty('sankeyWidth')) ? graph['sankeyWidth']: 700;
+
+    // Variables to hold sizing of the sankey
+    var margin = {top: 0, right: 100, bottom: 20, left: 0},
+      width = sankeyWidth - margin.left - margin.right,
+      height = 400 - margin.top - margin.bottom;
+
+
+    // format variables
+    var color = d3.scaleOrdinal(d3.schemeCategory20);
+
+    // I'll be honest. I have no clue here. 
+    var zoomListener = d3.zoom()
+      .scaleExtent([1, 5])
+      .on('zoom', zoomed);
+
+    function zoomed() {
+      if (d3.event.transform.k === 1) d3.event.transform.y = 0;
+      if (d3.event.transform.x > 0) d3.event.transform.x = 0;
+      svg.attr('transform', d3.event.transform);
+    }
+
+    // append the svg object to the body of the page
+    var svg = d3.select('#mySankeyDigram').append('svg')
+      .attr('width', width + margin.left + margin.right)
+      .attr('height', height + margin.top + margin.bottom)
+      .append('g')
+      .attr('transform',
+            'translate(' + margin.left + ',' + margin.top + ')')
+
+    zoomListener(svg);
+
+    // Set the sankey diagram properties
+    var sankey = d3.sankey()
+      .nodeWidth(10)
+      .nodePadding(30)
+      .size([width, height])
+      .overlapLinksAtSources(true)
+      //.overlapLinksAtTargets(true)
+      .align('right');
+
+    var path = sankey.link();
   
-  // D3 likes to high jack click events when we turn on zoom and drag. I use this variable
-  // to track how far we drag. If we drag 0 or less than a certain amount we pop up a modal 
-  // which simulates a click
-  let dragMovement=0;
+    // D3 likes to high jack click events when we turn on zoom and drag. I use this variable
+    // to track how far we drag. If we drag 0 or less than a certain amount we pop up a modal 
+    // which simulates a click
+    let dragMovement=0;
 
-  // Function that handles our dragging
-  var drag = d3.drag()
-    // If we are dragging we are calling 'dragmove' function
-    .on('drag', dragmove)
-    // If start the dragging
-    .on('start', function(){
-      this.parentNode.appendChild(this);
-    })
-    //When we end the drag movement
-    .on('end', function(d){
-      // This is the check to see how far we dragged. If we haven't gone far then
-      // we treat it as a click and open that modal
-      if (dragMovement <= 5) {
-        var mymodal = $('#modalContent');
-        mymodal.find('.modal-title').html('<h3>' + d.name + '</h3>');
-        mymodal.find('.modal-body').html(graph['flowInfo'][d.name]['html']);
-        mymodal.modal('show');
-      }
-      // Reset the drag movement for next time
-      dragMovement = 0;
-    });
+    // Function that handles our dragging
+    var drag = d3.drag()
+      // If we are dragging we are calling 'dragmove' function
+      .on('drag', dragmove)
+      // If start the dragging
+      .on('start', function(){
+        this.parentNode.appendChild(this);
+      })
+      //When we end the drag movement
+      .on('end', function(d){
+        // This is the check to see how far we dragged. If we haven't gone far then
+        // we treat it as a click and open that modal
+        if (dragMovement <= 5) {
+          openModal(d.name,graph['flowInfo'][d.name]['html'])
+        }
+        // Reset the drag movement for next time
+        dragMovement = 0;
+      });
 
-  // We add the nodes and the links to the sankey and let it iterate 200 times to make it pretty
-  sankey
-    .nodes(graph.nodes)
-    .links(graph.links)
-    .layout(200);
+    // We add the nodes and the links to the sankey and let it iterate 200 times to make it pretty
+    sankey
+      .nodes(graph.nodes)
+      .links(graph.links)
+      .layout(200);
 
-  //This creates the links to be drawn
-  var link = svg.append('g').selectAll('.link')
-    .data(graph.links)
-    .enter().append('path')
-    .attr('class', 'link')
-    .attr('d', path)
-    .style('stroke-width', function(d) { return Math.max(1, d.dy); })
-    .sort(function(a, b) { return b.dy - a.dy; })
-    // If we click the link between two nodes we pop up a modal
-    .on("click", function(d) {
-      var mymodal = $('#modalContent');
-      mymodal.find('.modal-title').html('<h3>Flow Schema</h3>');
-      var flowhtml = ''
-      if (graph['flowInfo'][d.source.name].hasOwnProperty('flowhtml')) {
-        flowhtml = graph['flowInfo'][d.source.name]['flowhtml'];
-      }
-      mymodal.find('.modal-body').html(flowhtml);
-      mymodal.modal('show');
-    });
+    //This creates the links to be drawn
+    var link = svg.append('g').selectAll('.link')
+      .data(graph.links)
+      .enter().append('path')
+      .attr('class', 'link')
+      .attr('d', path)
+      .style('stroke-width', function(d) { return Math.max(1, d.dy); })
+      .sort(function(a, b) { return b.dy - a.dy; })
+      // If we click the link between two nodes we pop up a modal
+      .on("click", function(d) {
+        openModal('<h3>Flow Schema</h3>',graph['flowInfo'][d.source.name]['flowhtml']);
+      });
 
-  // This gives a title to the link
-  link.append('title')
-    .text(function(d) {return d.source.name + ' → ' + d.target.name; });
+    // This gives a title to the link
+    link.append('title')
+      .text(function(d) {return d.source.name + ' → ' + d.target.name; });
 
-  // add in the nodes
-  var node = svg.append('g').selectAll('.node')
-    .data(graph.nodes)
-    .enter()
-    .append('g')
-    .attr('class', 'node')
-    .attr('transform', function(d) {
-      return 'translate(' + d.x + ',' + d.y + ')'; })
-    .call(drag);
+    // add in the nodes
+    var node = svg.append('g').selectAll('.node')
+      .data(graph.nodes)
+      .enter()
+      .append('g')
+      .attr('class', 'node')
+      .attr('transform', function(d) {
+        return 'translate(' + d.x + ',' + d.y + ')'; })
+      .call(drag);
 
-  // add the rectangles for the nodes
-  node.append('rect')
-    .attr('height', function(d) { return d.dy; })
-    .attr('width', sankey.nodeWidth())
-    .style('fill', function(d) {
-      return d.color = color(d.name.replace(/ .*/, '')); })
-    .style('stroke', function(d) {
-      return d3.rgb(d.color).darker(2); })
-    .append('title')
-    .text(function(d) {
-      return d.name + '\n' + d.dy; })
+    // add the rectangles for the nodes
+    node.append('rect')
+      .attr('height', function(d) { return d.dy; })
+      .attr('width', sankey.nodeWidth())
+      .style('fill', function(d) {
+        return d.color = color(d.name.replace(/ .*/, '')); })
+      .style('stroke', function(d) {
+        return d3.rgb(d.color).darker(2); })
+      .append('title')
+      .text(function(d) {
+        return d.name + '\n' + d.dy; })
 
-  // add in the title for the nodes
-  node.append('text')
-    .attr('x', -6)
-    .attr('y', function(d) { return d.dy / 2; })
-    .attr('dy', '.35em')
-    .attr('text-anchor', 'end')
-    .attr('transform', null)
-    .text(function(d) { return d.name; })
-    //.filter(function(d) { return d.x < width / 2; })
-    .attr('x', 6 + sankey.nodeWidth())
-    .attr('text-anchor', 'start');
+    // add in the title for the nodes
+    node.append('text')
+      .attr('x', -6)
+      .attr('y', function(d) { return d.dy / 2; })
+      .attr('dy', '.35em')
+      .attr('text-anchor', 'end')
+      .attr('transform', null)
+      .text(function(d) { return d.name; })
+      //.filter(function(d) { return d.x < width / 2; })
+      .attr('x', 6 + sankey.nodeWidth())
+      .attr('text-anchor', 'start');
 
-  // hide the progress bar
-  $('#work-in-progress').hide();
+    // hide the progress bar
+    $('#work-in-progress').hide();
 
-  // the function for moving the nodes
-  function dragmove(d) {
+    // the function for moving the nodes
+    function dragmove(d) {
     dragMovement++;
-    d3.select(this).attr('transform',
-      'translate(' + (
-        d.x = Math.max(0, Math.min(width - d.dx, d3.event.x))
-      ) + ',' + (
-        d.y = Math.max(0, Math.min(height - d.dy, d3.event.y))
-      ) + ')');
-    sankey.relayout();
-    link.attr('d', path);
-  }
+      d3.select(this).attr('transform',
+        'translate(' + (
+          d.x = Math.max(0, Math.min(width - d.dx, d3.event.x))
+        ) + ',' + (
+          d.y = Math.max(0, Math.min(height - d.dy, d3.event.y))
+        ) + ')');
+     sankey.relayout();
+      link.attr('d', path);
+    }
+  });
+}
 
-});
+function divError(title,body) {
+  // Hide the main content
+  var mycontainer = $('#mySankeyContainer')
+  mycontainer.hide();
 
+  // Show the alert
+  var myalert = $('#myAlert');
+  myalert.find('strong').html(title);
+  myalert.find('p').html(body);
+  myalert.removeAttr('class');
+  myalert.addClass('alert alert-danger alert-dismissable');
+  myalert.show();
+  return true;
+}
+
+function openModal(title,body) {
+  var mymodal = $('#modalContent');
+  mymodal.find('.modal-title').html('<h3>' + title + '</h3>');
+  mymodal.find('.modal-body').html(body);
+  mymodal.modal('show');
+  return true;
 }
 
 function sankeyReset() {
@@ -200,31 +203,23 @@ function sankeyReset() {
   svg.call(zoomListener.transform, t)
 }
 
+//This function makes an AJAX call to /sankey/data/getFeeds in order to get
+//the list of feeds that we can build a Sankey diagram for
 function populateDropDown() {
-  
-$.ajax({
-  type: "GET",
-  url: '/sankey/data/getFeeds',
-  success: function(data)
-  {
-    if(data.hasOwnProperty('error')) {
-      var myalert = $('#myAlert');
-      myalert.find('strong').html('Validation Error!');
-      myalert.find('p').html(data.error);
-      myalert.removeAttr('class');
-      myalert.addClass('alert alert-danger alert-dismissable');
-      myalert.show();
-      return;
+  $.ajax({
+    type: "GET",
+    url: '/sankey/data/getFeeds',
+    success: function(data)
+    {
+      if(data.hasOwnProperty('error')) {
+        return divError('Validation Error!', data.error);
+      }
+      var dropdown = $('#feedDropDown');
+      dropdown.html('');
+      data['feedOutputs'].forEach(function(feedOutput, i) {
+        var selectionHTML = '<a class="dropdown-item" href="#" onclick="drawFeedSankey(\'' + feedOutput['jobName'] + '\');">' + feedOutput['jobName'] + '</a>';
+        dropdown.append(selectionHTML);
+      });
     }
-    var dropdown = $('#feedDropDown');
-    dropdown.html('');
-    data['feedOutputs'].forEach(function(feedOutput, i) {
-      var selectionHTML = '<a class="dropdown-item" href="#" onclick="drawFeedSankey(\'' + feedOutput['jobName'] + '\');">' + feedOutput['jobName'] + '</a>';
-      dropdown.append(selectionHTML);
-    });
-    
-    
-  }
-});
-
+  });
 }
